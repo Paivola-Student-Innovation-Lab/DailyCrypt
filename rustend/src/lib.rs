a9416589f81e8252772234af347aa05696c6b09e
Daniel Kristiansson <daniel.kristiansson@psil.fi>
2024-05-09 13:02:50 +0300

    cargo fmt

--- rustend/src/lib.rs
+++ rustend/src/lib.rs
@@ -4,8 +4,7 @@ use wasm_bindgen::prelude::*;
 mod crypt;
 
 #[wasm_bindgen]
-extern {
-}
+extern "C" {}
 
 #[wasm_bindgen]
 pub fn encrypt(chunk: Vec<u8>, nonce_vec: Vec<u8>, keybytes: Vec<u8>) -> Vec<u8> {
@@ -18,21 +17,23 @@ pub fn encrypt(chunk: Vec<u8>, nonce_vec: Vec<u8>, keybytes: Vec<u8>) -> Vec<u8>
 }
 
 #[wasm_bindgen]
-pub fn decrypt(chunk: Vec<u8>, nonce_vec: Vec<u8>, keybytes: Vec<u8>) -> Vec<u8>  {
+pub fn decrypt(chunk: Vec<u8>, nonce_vec: Vec<u8>, keybytes: Vec<u8>) -> Vec<u8> {
     match crypt::decrypt_chunk(chunk, nonce_vec, keybytes) {
         Ok(result) => result,
-        Err(_err) => {
-            [].to_vec()
-        }
+        Err(_err) => [].to_vec(),
     }
 }
 
 #[wasm_bindgen]
 pub fn get_cipher_key(passwd: &str) -> Vec<u8> {
     let sha = sha256::digest(passwd); // Digest password (turn it into 64 hex bytes)
-    (0..sha.len()).step_by(2).map(|byte| u8::from_str_radix(&sha[byte..byte+2], 16).unwrap()).collect::<Vec<u8>>().try_into().unwrap() // Turn hex bytes into real bytes
+    (0..sha.len())
+        .step_by(2)
+        .map(|byte| u8::from_str_radix(&sha[byte..byte + 2], 16).unwrap())
+        .collect::<Vec<u8>>()
+        .try_into()
+        .unwrap() // Turn hex bytes into real bytes
 }
-
 
 #[wasm_bindgen]
 pub fn get_nonce(password: &str) -> Vec<u8> {
@@ -49,21 +50,25 @@ pub fn get_nonce(password: &str) -> Vec<u8> {
 mod tests {
     use std::str::from_utf8;
 
-    use crate::{crypt::{decrypt_chunk, encrypt_chunk, generate_nonce}, get_cipher_key};
+    use crate::{
+        crypt::{decrypt_chunk, encrypt_chunk, generate_nonce},
+        get_cipher_key,
+    };
 
-
-    fn encrypt_decrypt(plaintext: &str, nonce_vec: Vec<u8>, keybytes: Vec<u8>)  {
-        match encrypt_chunk(plaintext.as_bytes().to_vec(), nonce_vec.clone(), keybytes.clone()) {
-            Ok(t) => {
-                match decrypt_chunk(t, nonce_vec.clone(), keybytes.clone()) {
-                    Ok(m) => {
-                        let newplaintext = from_utf8(&m).unwrap();
-                        assert_eq!(plaintext, newplaintext);
-                    },
-                    Err(e) => println!("{}", e),
+    fn encrypt_decrypt(plaintext: &str, nonce_vec: Vec<u8>, keybytes: Vec<u8>) {
+        match encrypt_chunk(
+            plaintext.as_bytes().to_vec(),
+            nonce_vec.clone(),
+            keybytes.clone(),
+        ) {
+            Ok(t) => match decrypt_chunk(t, nonce_vec.clone(), keybytes.clone()) {
+                Ok(m) => {
+                    let newplaintext = from_utf8(&m).unwrap();
+                    assert_eq!(plaintext, newplaintext);
                 }
-            }
-            Err(e) => println!("{}", e)
+                Err(e) => println!("{}", e),
+            },
+            Err(e) => println!("{}", e),
         }
     }
 
